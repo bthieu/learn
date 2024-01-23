d924334aefd4ef79880765fb3ad1a86a6b948a26
Hieu Bui <tranhieu0101@gmail.com>
2024-01-23 21:27:46 +0700

    form with zod

--- react-hook-form/src/app/page.tsx
+++ react-hook-form/src/app/page.tsx
@@ -1,11 +1,13 @@
 import SimpleForm from "./components/simple-form";
 import FormWithReactHookForm from "./components/form-with-react-hook-form";
+import FormWithReactHookFormAndZod from "./components/form-with-rhf-and-zod";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <SimpleForm /> */}
-      <FormWithReactHookForm />
+      {/* <FormWithReactHookForm /> */}
+      <FormWithReactHookFormAndZod />
     </main>
   )
 }
